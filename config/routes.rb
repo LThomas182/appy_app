f419bb05dbdcb5f75389f5f0b4c8ca48ea136cf9
Linda Thomas <linda.thomas.cmyevents@gmail.com>
2014-03-20 15:53:04 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 AppyApp::Application.routes.draw do
+  match '/signup',  to: 'users#new',            via: 'get'
+
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
